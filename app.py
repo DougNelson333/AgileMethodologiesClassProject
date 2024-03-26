dd673ab68e24dcb28616fdd4c112f17a030dfb82
Gamerplayer22 <kalman.lengyel@trojans.dsu.edu>
2024-03-26 18:42:46 -0500

    register,login,logout
    register, login and logout implemented
    database is working good

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 
 #database setup
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
-db = SQLAlchemy(app)
+db = SQLAlchemy()
 
 #configurations
 app.config['SECRET_KEY']= 'your secret key'
@@ -25,28 +25,17 @@ login_manager.login_view = "login"
 
 #login manager user load from database
 @login_manager.user_loader
-def load_user():
-    return None
+def load_user(user_id):
+    return Users.query.get(user_id)
 
 #user class
-class Users(db.Model):
+class Users(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(25), nullable=False)
     email = db.Column(db.String(40), nullable=False, unique=True)
     password = db.Column(db.String(255), nullable=False)
 
-    def get_id(self):
-        return str(self.id)
-
-    def is_active(self):
-        return True
-
-    def is_authenticated(self):
-        return True
-
-    def is_anonymous(self):
-        return False
-
+db.init_app(app)
 
 #main page
 @app.route('/')
@@ -54,10 +43,53 @@ def index():
     return render_template('index.html')
 
 #register
+@app.route('/register',methods=['GET','POST'])
+def register():
+    if request.method=='POST':
+        username=request.form['username']
+        email=request.form['email']
+        password=request.form['password']
+        c_password=request.form['confirm_password']
+
+        user=Users.query.filter_by(email=email).first()
+        
+        if not is_password_strong(password):
+            flash('Password is not strong enough!','error')
+        elif password != c_password:
+            flash('Passwords do not match!','error')
+        elif user != None:
+            flash('User already exists!','error')
+        else:
+            hash_password=generate_password_hash(password)
+            #insert database
+            user = Users(username=username,email=email,password=hash_password)
+            db.session.add(user)
+            db.session.commit()
+            flash('Registration successful!','success')
+            return render_template('index.html')
+    return render_template('register.html')
 
 #login
+@app.route('/login',methods=['GET','POST'])
+def login():
+    if request.method=='POST':
+        email=request.form['email']
+        password=request.form['password']
+        user = Users.query.filter_by(email=email).first()
+        if user and check_password_hash(user.password, password):
+            login_user(user,remember=True)
+            flash('Login successful!','success')
+            return redirect(url_for('index'))
+        else:
+            flash('Invalid email or password!','error')
+            return redirect(url_for('login'))
+    return render_template('login.html')
 
 #logout
+@app.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for("index"))
 
 #show user's subscriptions
 
@@ -67,6 +99,11 @@ def index():
 
 #add a subsciption
 
+#password strength check
+def is_password_strong(password):
+    # Minimum 8 characters, at least one uppercase letter, one lowercase letter, one number, and one special character
+    pattern = r'(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[\W_]).{8,}'
+    return re.match(pattern, password) is not None
 
 if __name__ =='__main__':
     app.run(debug=True)
